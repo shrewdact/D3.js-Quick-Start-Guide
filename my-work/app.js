9eb0d2c815e49784b87e209f10ced80fa90ede7f
JuSeonghyeok <shrewdact@gmail.com>
2019-01-06 14:56:08 +0900

    Done making a donut graph

--- my-work/app.js
+++ my-work/app.js
@@ -20,14 +20,14 @@ colorScale.domain(
 
 d3.select('svg')
   .attr('width', WIDTH)
-  .attr('height',HEIGHT);
+  .attr('height', HEIGHT);
 var container = d3
   .select('g') //add this line and the next:
-  .attr('transform', 'translate(' + (WIDTH / 2) + ',' + (HEIGHT / 2) + ')'); //add this line
+  .attr('transform', 'translate(' + WIDTH / 2 + ',' + HEIGHT / 2 + ')'); //add this line
 
 var arc = d3
   .arc()
-  .innerRadius(0) //to make this a donut graph, adjust this value
+  .innerRadius(100) //to make this a donut graph, adjust this value
   .outerRadius(radius);
 
 var pie = d3
